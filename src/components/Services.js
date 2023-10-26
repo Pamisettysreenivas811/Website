8d77f1bcf9628d0dd22b9dd451a5a17f521e56c8
Pamisettysreenivas811 <134376289+Pamisettysreenivas811@users.noreply.github.com>
2023-10-26 12:45:07 +0530

    Commit Image Change

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 import img1 from './images/img10.jpeg';
-import img4 from './images/img11.jpeg'
+import img4 from './images/img4.jpeg';
+import img5 from './images/img5.jpeg';
+import img6 from './images/img6.jpeg';
+import img7 from './images/img7.jpeg';
+import img8 from './images/img8.jpeg';
+import img9 from './images/img9.jpeg';
 
 import ServiceCard from "./ServiceCard";
 
@@ -18,14 +23,14 @@ let data = [
     description:
       "We can assist you in ensuring that your IT is able to quickly and efficiently adapt to new business requirements.Consultant is usually an expert or an experienced professional in a specific field and has a wide knowledge.",
     height: "280px" ,
-    imgpath: "https://live.staticflickr.com/65535/53059334555_1e72bf30e1_m.jpg",
+    imgpath: "img5",
   },
   {
     title: "Medical Coding Services",
     description:
       "Coding service is a key activity that has a direct impact on the financial status of your practice. We have specialized expertise, niche focus, and infrastructure to support your requirements.Get access to certified coders with experience in outpatient, inpatient, & emergency department coding.",
     height: "255px",
-    imgpath: "https://live.staticflickr.com/65535/53059493593_3b34cb2a7a_m.jpg",
+    imgpath: "img6",
     readmoreUrl: "/services/medicalcode"
   },
   {
@@ -33,21 +38,21 @@ let data = [
     description:
       "Aryavart design and create fields involved in the planning, creation, and arrangement of visual elements to communicate a message or convey a specific idea. create visually appealing and impactful designs.",
     height: "330px",
-    imgpath: "https://live.staticflickr.com/65535/53059133734_068075acc7_m.jpg",
+    imgpath: "img7",
   },
   {
     title: "Mobile development",
     description:
       " We offer a full cycle of application design, integration and management services. Whether it is a consumer oriented app or a transformative enterprise-class solution, the company leads the entire mobile app development process from ideation and concept to delivery, and to ongoing ongoing support.",
     height: "280px",
-    imgpath: "https://live.staticflickr.com/65535/53059334495_8f3727e350_m.jpg",
+    imgpath:"img8",
   },
   {
     title: "Staffing",
     description:
       "We are serving various industries and startups in Hospitality, Education, Edtech, E-Commerce, and many more. Where we cater them from entry-level to mid-level positions. We would be glad to serve your organisation as well and would like to be a part of your growth journey.",
     height: "280px",
-    imgpath: "https://live.staticflickr.com/65535/53058366707_2711b8c532_m.jpg",
+    imgpath: "img9",
   },
 ];
 
